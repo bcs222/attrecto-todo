d1dc3bf1477d5de34c422ca761dfaaf8329a766a
csaba.botos <email@example.com>
2020-10-29 11:48:17 +0100

    Spring Boot Starter App -- check JSP response

---
+++ src/main/java/com/attrecto/AttrectoTodoApplication.java
@@ -0,0 +1,13 @@
+package com.attrecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttrectoTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttrectoTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attrecto/AttrectoTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attrecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttrectoTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
